7f524fb40dc4014769098d855896defafb65aa29
murawaki-村脇 <mitsuhiromurawaki@gmail.com>
2018-02-21 19:47:37 +0900

    Add setup-contos.sh

---
+++ setup-centos.sh
@@ -0,0 +1,14 @@
+### install zsh
+yum -y install zsh
+
+### set login shell
+chsh -s /bin/zsh
+
+### download setting files
+wget https://raw.githubusercontent.com/murawakimitsuhiro/shell-config/master/.tigrc
+wget https://raw.githubusercontent.com/murawakimitsuhiro/shell-config/master/.vimrc
+wget https://raw.githubusercontent.com/murawakimitsuhiro/shell-config/master/.zshrc
+
+### install tig
+rpm -ivh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
+yum install -y tig
