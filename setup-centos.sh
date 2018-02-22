89ba78787d60c587571e7ea28e2f8d555ecfba18
murawaki-村脇 <mitsuhiromurawaki@gmail.com>
2018-02-22 15:30:49 +0900

    Update wgetで上書きするように

--- setup-centos.sh
+++ setup-centos.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
-
 ### install zsh
 sudo yum -y install zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
@@ -14,11 +13,11 @@ chsh -s /bin/zsh
 
 ### download setting files
 cd ~/
-wget https://raw.githubusercontent.com/murawakimitsuhiro/shell-config/master/.tigrc
-wget https://raw.githubusercontent.com/murawakimitsuhiro/shell-config/master/.vimrc
-wget https://raw.githubusercontent.com/murawakimitsuhiro/shell-config/master/.zshrc
-wget https://raw.githubusercontent.com/murawakimitsuhiro/shell-config/master/.dein.toml
-wget https://raw.githubusercontent.com/murawakimitsuhiro/shell-config/master/.dein_lazy.toml
+wget -r https://raw.githubusercontent.com/murawakimitsuhiro/shell-config/master/.tigrc
+wget -r https://raw.githubusercontent.com/murawakimitsuhiro/shell-config/master/.vimrc
+wget -r https://raw.githubusercontent.com/murawakimitsuhiro/shell-config/master/.zshrc
+wget -r https://raw.githubusercontent.com/murawakimitsuhiro/shell-config/master/.dein.toml
+wget -r https://raw.githubusercontent.com/murawakimitsuhiro/shell-config/master/.dein_lazy.toml
 source ~/.zshrc
 source ~/.tigrc
 source ~/.vimrc
@@ -26,3 +25,4 @@ source ~/.vimrc
 ### install tig
 sudo rpm -ivh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
 sudo yum install -y tig
+
