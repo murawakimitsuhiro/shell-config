2c8c012d4400af266edaf9ac1b1602b8e0ff8e10
murawaki-村脇 <mitsuhiromurawaki@gmail.com>
2018-02-22 15:24:24 +0900

    Add install oh-my-zsh

--- setup-centos.sh
+++ setup-centos.sh
@@ -5,6 +5,10 @@
 sudo yum -y install zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
+### install oh-my-zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
 ### set login shell
 chsh -s /bin/zsh
 
